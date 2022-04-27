16f965943b766ba79067ae8c2bfa21baf30f8185
Christian NGAPGUE <christianngapgue@gmail.com>
2022-04-27 20:21:22 +0100

    Use the handleDelete function props of BlogList

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,13 @@ function Home() {
     {title: 'Welcome', body: 'lorem ipsum...', author: 'yoshi', id: 2},
     {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
   ])
+
+  const deleteBlog = (blogId) => {
+    setBlogs(blogs.filter((blog) => blog.id !== blogId))
+  }
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs"/>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={deleteBlog}/>
     </div>
   );
 }
