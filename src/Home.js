73d761c8ebece3e599a1ba09610f3adebbedc8eb
Christian NGAPGUE <christianngapgue@gmail.com>
2022-04-28 23:25:49 +0100

    Use the useEffect hook
    ...for running code at every render of the component it is in.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
 import React from "react";
-import {useState} from 'react'
-import BlogList from './BlogList'
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 function Home() {
   const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    {title: 'Welcome', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
-  ])
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   const deleteBlog = (blogId) => {
-    setBlogs(blogs.filter((blog) => blog.id !== blogId))
-  }
+    setBlogs(blogs.filter((blog) => blog.id !== blogId));
+  };
+
+  useEffect(() => {
+    console.log("use effect ran");
+  });
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={deleteBlog}/>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={deleteBlog} />
     </div>
   );
 }
