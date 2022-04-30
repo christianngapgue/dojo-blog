aae4f6e022b89933db1fdc6ef51797054e1ea18d
Christian NGAPGUE <christianngapgue@gmail.com>
2022-04-30 23:40:52 +0100

    Use conditional message loading to display the error message

--- src/Home.js
+++ src/Home.js
@@ -5,30 +5,35 @@ import BlogList from "./BlogList";
 function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
-      .then((res) => {
-        console.log(res);
-        if(!res.ok) {
-          throw Error('could not fetch the data for that resource')
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-    }, 1000); 
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
