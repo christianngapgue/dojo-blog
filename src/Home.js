c83fec671573e9e1afbca1fe80fe2c463e5979d7
Christian NGAPGUE <christianngapgue@gmail.com>
2022-04-30 23:16:46 +0100

    Add a conditional loading message
    ...to display when the data is still being retrieved from the data
    source.

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 
 function Home() {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
@@ -13,11 +14,13 @@ function Home() {
       .then((data) => {
         console.log(data);
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
