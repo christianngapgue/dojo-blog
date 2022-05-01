b40b9165c661ae27c7b284b698210c36b14341f8
Christian NGAPGUE <christianngapgue@gmail.com>
2022-05-01 22:25:49 +0100

    Create a custom hook useFetch
    ...to fetch some data from an endpoint url.

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,15 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 function Home() {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          console.log(res);
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          console.log(err.message);
-          setError(err.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
+  const {data, isPending, error} = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 }
