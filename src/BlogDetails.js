efce45974a908db0947ad8ad6073890b78a334fb
Christian NGAPGUE <christianngapgue@gmail.com>
2022-05-15 20:38:08 +0100

    Display the id field of the Blog data
    ...for a particular blog.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function BlogDetails() {
+  const { blogId } = useParams();
+  
   return (
     <div id="blog-details">
-      <h2>Blog Details</h2>
+      <h2>Blog Details - {blogId}</h2>
     </div>
   );
 }
