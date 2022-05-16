1d1388f6c2fc23ffe62472da9cba116af23c6a84
Christian NGAPGUE <christianngapgue@gmail.com>
2022-05-16 16:25:54 +0100

    Fetch blog details data
    ...to display in the <BlogDetails /> componenent.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 function BlogDetails() {
   const { blogId } = useParams();
-  
+  const { data, error, isPending } = useFetch(
+    "http://localhost:8000/blogs/" + blogId
+  );
+
   return (
     <div id="blog-details">
-      <h2>Blog Details - {blogId}</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2>{data.title}</h2>
+          <p>Written by {data.author}</p>
+          <div>{data.body}</div>
+        </article>
+      )}
     </div>
   );
 }
