efd4d38c75898407e9e419361b5e0cb6edf059b6
Christian NGAPGUE <christianngapgue@gmail.com>
2022-05-16 17:02:56 +0100

    Create a form in the <Create /> component

--- src/Create.js
+++ src/Create.js
@@ -4,6 +4,18 @@ function Create() {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required />
+        <label>Blog body:</label>
+        <textarea required />
+        <label>Blog author:</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 }
