20ab54cf9c2673a190f13f3c7e2af6988bd85102
Christian NGAPGUE <christianngapgue@gmail.com>
2022-05-16 17:36:45 +0100

    Add a message while the blog creation is ongoing

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,12 @@ function Create() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+    setIsPending(true);
 
     fetch("http://localhost:8000/blogs", {
       method: "POST",
@@ -16,6 +18,7 @@ function Create() {
       body: JSON.stringify(blog),
     }).then(() => {
       console.log("New blog added");
+      setIsPending(false);
     });
   };
 
@@ -41,7 +44,8 @@ function Create() {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog</button>}
       </form>
     </div>
   );
